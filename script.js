735363aeabe4bb305c77491112115194c0827715
sahidulcse12 <m.sahidulislam.14@gmail.com>
2021-05-24 16:50:53 +0600

    timer

---
+++ script.js
@@ -0,0 +1,33 @@
+const daysE1 = document.getElementById('days');
+const hoursE1 = document.getElementById('hours');
+const minutesE1 = document.getElementById('mins');
+const secondsE1 = document.getElementById('sec');
+
+
+const newYears = '1 Jan 2022';
+
+function countdown() {
+    const newYearsDate = new Date(newYears);
+    const currentDate = new Date();
+
+    const totalSeconds = (newYearsDate - currentDate) / 1000;
+
+    const days = Math.floor(totalSeconds / 3600 / 24);
+    const hours = Math.floor(totalSeconds / 3600) % 24;
+    const minutes = Math.floor(totalSeconds / 3600) % 60;
+    const seconds = Math.floor(totalSeconds % 60);
+
+
+
+    daysE1.innerHTML = days;
+    hoursE1.innerHTML = hours;
+    minutesE1.innerHTML = minutes;
+    secondsE1.innerHTML = seconds;
+
+
+}
+
+
+countdown();
+
+setInterval(countdown, 1000);
